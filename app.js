261cac21f6b35cd2c5df22a71080b9bee8524804
Stuart Zahn <stuart.zahn@gmail.com>
2014-06-03 00:59:16 -0700

    Added url path for screenshot generation

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 var http = require('http'),
-	express = require('express');
+	express = require('express'),
+	gm = require('gm');
 
 var app = express();
 
-app.get('/generate', function(req, res){
-	//Generate the mobile screenshot here by calling image magick
+app.get('/generate/:device/:orientation', function(req, res){
+
+	var devices = {"android-v": {width: 0, height: 0, image: ""},
+		"android-h": { width: 0, height: 0, image: ""},
+		"iphone-v": { width: 0, height: 0, image: ""},
+		"iphone-h": { width: 0, height: 0, image: ""}};
+
+	var deviceName = req.params.device;
+	var orientation = req.params.orientation;
+
+	var deviceKey = deviceName + "-" + orientation;
+
+	var targetDevice = devices[deviceKey];
+	if (typeof(targetDevice) == "undefined"){
+		throw "Device " + deviceKey + " not recognized";
+	}
+
+	var filename = req.query.file;
+
+	console.log("Generating %s for %s", filename, deviceKey);
+
+	//var filename = eq.params.filename;
 
 	res.send('generated'); //Test response for now
 });
